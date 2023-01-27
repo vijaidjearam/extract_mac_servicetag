04a152028e7f9fd806ccabf4d173d54159afbc76
vijai <vijai.djearam@gmail.com>
2023-01-27 12:06:27 +0100

    Create startup_script.ps1

---
+++ startup_script.ps1
@@ -0,0 +1,7 @@
+Set-ExecutionPolicy Bypass -Scope Process -Force; [System.Net.ServicePointManager]::SecurityProtocol = [System.Net.ServicePointManager]::SecurityProtocol -bor 3072; iex ((New-Object System.Net.WebClient).DownloadString('https://community.chocolatey.org/install.ps1'))
+Invoke-WebRequest -Uri "https://raw.githubusercontent.com/vijaidjearam/extract_mac_servicetag/main/extract-macadress-servicetag.py" -OutFile "C:\Users\WDAGUtilityAccount\Desktop\extract-macadress-servicetag.py"
+choco install -y python3 tesseract
+python.exe -m pip install --upgrade pip
+pip install pytesseract pillow
+[System.Environment]::SetEnvironmentVariable('TESSDATA_PREFIX','C:\Program Files\Tesseract-OCR\tessdata')
+New-Item -ItemType Directory -Path c:\ -Name temp
